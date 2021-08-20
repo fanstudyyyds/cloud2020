4bb00e8d3dc56ce94b53bc615837a4d10497f376
fanstudy <2864169033@qq.com>
2021-08-20 17:36:44 +0800

    Nacos之服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/fan/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.fan.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //支持Nacos的动态刷新功能。
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
