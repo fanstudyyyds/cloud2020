b67fb18b23d3b997353c1ae32d998da362553e46
fanstudy <2864169033@qq.com>
2021-08-21 10:34:10 +0800

    Sentinel流控-QPS直接失败

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/fan/springcloud/MainApp8401.java
@@ -0,0 +1,13 @@
+package com.fan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/fan/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,27 @@
+package com.fan.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+        return "------testB";
+    }
+}
